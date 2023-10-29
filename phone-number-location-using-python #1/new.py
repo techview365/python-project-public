b048bdaff46186b9b8e4104008bf2109b61cb8bc
Manish <manish.dinkar13@gmail.com>
2023-10-29 23:14:18 +0530

    complete file

---
+++ phone-number-location-using-python #1/new.py
@@ -0,0 +1,29 @@
+import phonenumbers
+from phonenumbers import geocoder
+import folium
+from phonenumbers import carrier
+from opencage.geocoder import OpenCageGeocode
+
+Key = "dcca7aaf75a345d2ab35a11405f79015"
+
+number = input("Enter phone number with country code: ")
+check_number = phonenumbers.parse(number)
+number_location = geocoder.description_for_number(check_number, "en")
+print(number_location)
+
+
+service_provider = phonenumbers.parse(number)
+print(carrier.name_for_number(service_provider, "en"))
+
+geocoder = OpenCageGeocode(Key)
+
+query = str(number_location)
+results = geocoder.geocode(query)
+
+lat = results[0]['geometry']['lat']
+lng = results[0]['geometry']['lng']
+print(lat, lng)
+
+map_location = folium.Map(location = [lat, lng], zoom_start=9)
+folium.Marker([lat, lng], popup=number_location).add_to(map_location)
+map_location.save("mylocation.html")
---
+++ phone-number-location-using-python #1/readme.md
@@ -0,0 +1,8 @@
+#Install package
+pip install phonenumbers
+pip install folium
+pip install opencage
+
+I have install these packages
+
+
